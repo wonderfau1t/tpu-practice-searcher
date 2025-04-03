88d4d37edeb44c087360268e38027f139946290f
wonderfau1t <wonderrfau1t@gmail.com>
2025-04-03 14:56:20 +0700

    fix: / endpoint added

--- cmd/tpu-practice-searcher/main.go
+++ cmd/tpu-practice-searcher/main.go
@@ -4,13 +4,11 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
+	"github.com/go-chi/render"
 	"net/http"
-	"os"
 	"tpu-practice-searcher/internal/config"
-	"tpu-practice-searcher/internal/http-server/handlers"
 	"tpu-practice-searcher/internal/http-server/middlewares"
 	"tpu-practice-searcher/internal/logger"
-	"tpu-practice-searcher/internal/storage/postgresql"
 )
 
 func main() {
@@ -18,12 +16,12 @@ func main() {
 	log := logger.SetupLogger(cfg.Env)
 	log.Info("Config successfully loaded")
 
-	db, err := postgresql.SetupStorage(cfg.Storage)
-	if err != nil {
-		log.Error("Failed to set storage")
-		os.Exit(1)
-	}
-	log.Info("Storage successfully set")
+	//db, err := postgresql.SetupStorage(cfg.Storage)
+	//if err != nil {
+	//	log.Error("Failed to set storage")
+	//	os.Exit(1)
+	//}
+	//log.Info("Storage successfully set")
 
 	router := chi.NewRouter()
 
@@ -38,8 +36,11 @@ func main() {
 		MaxAge:           300,
 	}))
 
-	router.Get("/auth", handlers.Auth(log, db))
-	router.Get("/register", handlers.RegisterStudent(log, db))
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		render.JSON(w, r, map[string]string{"message": "Hello world!"})
+	})
+	//router.Get("/auth", handlers.Auth(log, db))
+	//router.Get("/register", handlers.RegisterStudent(log, db))
 
 	http.ListenAndServe("0.0.0.0:8000", router)
 }
