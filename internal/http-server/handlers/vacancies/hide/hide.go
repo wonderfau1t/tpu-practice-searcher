3fa62453067f337aeb2053fb67e9b3a4ae0b11bd
wonderfau1t <wonderrfau1t@gmail.com>
2025-05-10 13:22:05 +0700

    allow admin role to access vacancy details

--- internal/http-server/handlers/vacancies/hide/hide.go
+++ internal/http-server/handlers/vacancies/hide/hide.go
@@ -27,7 +27,7 @@ func New(log *slog.Logger, db Storage) http.HandlerFunc {
 			return
 		}
 
-		if claims.Role == "HR" || claims.Role == "headHR" {
+		if claims.Role == "HR" || claims.Role == "headHR" || claims.Role == "admin" {
 			vacancyId, err := strconv.ParseUint(chi.URLParam(r, "id"), 10, 64)
 			if err != nil || vacancyId == 0 {
 				log.Info("invalid id")
